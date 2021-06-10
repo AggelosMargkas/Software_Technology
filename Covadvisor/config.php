4d004530dab7f4edb76450ac05ccddc9bb54f478
cgiannos <37047743+cgiannos@users.noreply.github.com>
2021-06-10 19:26:28 +0300

    Add files via upload

---
+++ Covadvisor/config.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "covadvisor";
+
+            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+            ?>
