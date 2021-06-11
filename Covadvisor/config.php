486c9d8dfd618ffd4187f70a81a89b9af35c6638
AggelosMargkas <39811197+AggelosMargkas@users.noreply.github.com>
2021-06-11 20:23:59 +0300

    ΜΠΑΥ

--- Covadvisor/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-        $dbUsername = "root";
-        $dbPassword = "";
-        $dbName = "covadvisor";
-
-            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-            ?>
